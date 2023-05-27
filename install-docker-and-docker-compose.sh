105ce5881e725f10ef58d7d9ebf8109bdcbb9395
Versio.io <57546250+versio-io@users.noreply.github.com>
2023-05-27 16:32:09 +0200

    Update install-docker-and-docker-compose.sh
    Optimize it for docker includes docker compose

--- install-docker-and-docker-compose.sh
+++ install-docker-and-docker-compose.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-echo '========== Cleanup environment & install docker and docker compose =========='
+echo '========== Cleanup environment & newest install docker =========='
 
 echo "Start to clean the environment from docker"
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update -y
+apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
 echo "Finished the cleaning from docker"
 
 echo "Start to install docker"
-sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-apt-cache -y madison docker-ce
-when running docker
-sudo usermod -aG docker $(whoami)
-echo "Finished the installation of docker"
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "Start to install docker compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo "Finished the installation of docker compose"
+echo "Finished the installation of docker"
 
 echo '============================ Finished ================================'
